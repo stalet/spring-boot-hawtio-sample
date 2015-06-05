4a19ee91fb3a9f876b8a106cc307ce7bc3177919
Ståle Tomten <staale@tomten.org>
2015-06-05 10:16:17 +0200

    Bumped to version 2.0.2-STALET and removed configuration that is now a part of hawtio.

--- src/main/java/org/tomten/samples/springboot/Application.java
+++ src/main/java/org/tomten/samples/springboot/Application.java
@@ -2,17 +2,45 @@ package org.tomten.samples.springboot;
 
 import io.hawt.config.ConfigFacade;
 import io.hawt.springboot.HawtPlugin;
+import io.hawt.springboot.HawtioConfiguration;
 import io.hawt.springboot.PluginService;
+import io.hawt.system.ConfigManager;
+import io.hawt.web.AuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+
 @SpringBootApplication
 @EnableAutoConfiguration
 @Import(HawtioConfiguration.class)
 public class Application {
+
+    @Autowired
+    private ServletContext servletContext;
+
+
+    /**
+     * Application main.
+     *
+     * @param args application arguments.
+     */
+    public static void main(String[] args) {
+        System.setProperty(AuthenticationFilter.HAWTIO_AUTHENTICATION_ENABLED, "false");
+        SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        final ConfigManager configManager = new ConfigManager();
+        configManager.init();
+        servletContext.setAttribute("ConfigManager", configManager);
+    }
 
     /**
      * Loading an example plugin
@@ -47,14 +75,4 @@ public class Application {
     public PluginService pluginService(){
         return new PluginService();
     }
-
-    /**
-     * Application main.
-     *
-     * @param args application arguments.
-     */
-    public static void main(final String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
 }
